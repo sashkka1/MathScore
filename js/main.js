38e9e1496e6bb0f29f1cc4a00c8514b28a8919c9
sashkka1 <84565833+sashkka1@users.noreply.github.com>
2024-04-19 18:17:10 +0300

    .

--- js/main.js
+++ js/main.js
@@ -79,7 +79,8 @@ function start(){
         }
     }
     sessionStorage.setItem('forScore',forScore);
-    document.location.href = 'http://127.0.0.1:5501/MathScore/html/add.html'
+    // document.location.href = 'http://127.0.0.1:5501/MathScore/html/add.html'
+    document.location.href == 'https://sashkka1.github.io/MathScore/index.html'
     score = 0;
     setExample();
     seconds = 0;
@@ -297,3 +298,87 @@ function blink(input, color){
         inpu.style.transition = "0.4s";
     }, 400);
 }
+
+
+jQuery(document).ready(function() {
+	$('.upper').on('input', setFill);
+	$('.lower').on('input', setFill);
+
+	var max = $('.upper').attr('max');
+	var min = $('.lower').attr('min');
+
+	function setFill(evt) {
+		var valUpper = $('.upper').val();
+		var valLower = $('.lower').val();
+		if (parseFloat(valLower) > parseFloat(valUpper)) {
+			var trade = valLower;
+			valLower = valUpper;
+			valUpper = trade;
+		}
+		
+		var width = valUpper * 100 / max;
+		var left = valLower * 100 / max;
+		$('.fill').css('left', 'calc(' + left + '%)');
+		$('.fill').css('width', width - left + '%');
+		
+		// Update info
+		if (parseInt(valLower) == min) {
+			$('.easy-basket-lower').val('0');
+		} else {
+			$('.easy-basket-lower').val(parseInt(valLower));
+		}
+		if (parseInt(valUpper) == max) {
+			$('.easy-basket-upper').val('300');
+		} else {
+			$('.easy-basket-upper').val(parseInt(valUpper));
+		}
+		$('.histogram-list li').removeClass('ui-histogram-active');
+	}
+	
+	// изменяем диапазон цен вручную
+	$('.easy-basket-filter-info p input').keyup(function() {
+		var valUpper = $('.easy-basket-upper').val();
+		var valLower = $('.easy-basket-lower').val();
+		var width = valUpper * 100 / max;
+		var left = valLower * 100 / max;
+		if ( valUpper > 300 ) {
+			var left = max;
+		}
+		if ( valLower < 0 ) {
+			var left = min;
+		} else if ( valLower > max ) {
+			var left = min;
+		}
+		$('.fill').css('left', 'calc(' + left + '%)');
+		$('.fill').css('width', width - left + '%');
+		// меняем положение ползунков
+		$('.lower').val(valLower);
+		$('.upper').val(valUpper);
+	});
+	$('.easy-basket-filter-info p input').focus(function() {
+		$(this).val('');
+	});
+	$('.easy-basket-filter-info .iLower input').blur(function() {
+		var valLower = $('.lower').val();
+		$(this).val(Math.floor(valLower));
+	});
+	$('.easy-basket-filter-info .iUpper input').blur(function() {
+		var valUpper = $('.upper').val();
+		$(this).val(Math.floor(valUpper));
+	});
+	
+	$('.histogram-list li').click(function() {
+		$('.histogram-list li').removeClass('ui-histogram-active');
+		var range_from = $(this).attr('price-range-from');
+		var range_to = $(this).attr('price-range-to');
+		var width = range_to * 100 / max;
+		var left = range_from * 100 / max;
+		$('.easy-basket-lower').val(range_from);
+		$('.easy-basket-upper').val(range_to);
+		$('.fill').css('left', 'calc(' + left + '%)');
+		$('.fill').css('width', width - left + '%');
+		$('.lower').val(range_from);
+		$('.upper').val(range_to);
+		$(this).addClass('ui-histogram-active');
+	});
+});
