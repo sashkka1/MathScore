ddad781f092490d2f9dad6eb814edda55b61a9c8
sashkka1 <84565833+sashkka1@users.noreply.github.com>
2024-10-24 19:42:32 +0300

    /

--- js/main.js
+++ js/main.js
@@ -25,14 +25,14 @@ window.onload = function () {
 
         document.getElementById('notificationp').innerText = "Не удалось получить информацию о пользователе.";
 
-        const user = Telegram.WebApp.initDataUnsafe.user;
+        // const user = Telegram.WebApp.initDataUnsafe.user;
         
-        if (user) {
-            const username = user.username || "Имя пользователя недоступно";
-            document.getElementById('notificationp').innerText = `Ваше имя пользователя: ${username}`;
-        } else {
-            document.getElementById('notificationp').innerText = "Не удалось получить информацию о пользователе.2";
-        }
+        // if (user) {
+        //     const username = user.username || "Имя пользователя недоступно";
+        //     document.getElementById('notificationp').innerText = `Ваше имя пользователя: ${username}`;
+        // } else {
+        //     document.getElementById('notificationp').innerText = "Не удалось получить информацию о пользователе.2";
+        // }
 
         // let notification = document.getElementById('example');
         // notification.outerHTML = `<p id="example">sdfsdf = </p>`;
